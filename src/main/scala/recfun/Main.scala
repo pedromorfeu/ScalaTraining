8f8b6fe84adadeb149f91399e384abe874466c83
Pedro Mano Fernandes <pedromorfeu@gmail.com>
2018-07-16 17:38:46 +0200

    Added solution for recursion exercises 1 and 2

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -17,17 +17,45 @@ object Main {
     if (c == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
-  println(pascal(0, 2))
-  println(pascal(1, 2))
-  println(pascal(1, 3))
-
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def balanceExtra(c: Char, chars: List[Char], count: Int): Int = {
+
+      val res = if (c == '(') 1 else if (c == ')') -1 else 0
+
+      if (count < 0) -1
+      else if (chars.isEmpty) count + res
+      else balanceExtra(chars.head, chars.tail, count + res)
+
+    }
+
+    if (chars.isEmpty) true
+    else balanceExtra(chars.head, chars.tail, 0) == 0
+
+  }
 
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    //    def counter(money: Int, coins: List[Int], count: Int): Int = {
+    //      if (money == count) 1
+    //      else if (coins.isEmpty) 0
+    //
+    //      if(counter(money, coins, coins.head)
+    //
+    //    }
+    //
+    //    counter(money, coins.tail, coins.head)
+
+    1
+
+  }
+
+  println(countChange(4, List(1, 2)))
+
 }
